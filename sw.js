b4fe4357cd45422a2a2bef7defb32ee93fbe0fcc
Kee Sern <chuakeesern@gmail.com>
2017-11-17 13:44:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.addEventListener('fetch', function(event) {
         });
         return response;
       }).catch(function () {
-        return caches.match('/sw-test/gallery/myLittleVader.jpg');
+        return caches.match('/keesern.github.io/unstoppableLunch2.jpg');
       });
     }
   }));
