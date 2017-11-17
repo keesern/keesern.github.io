883c2f863d96d4edcd05869bbd2db79fc373c73e
Kee Sern <chuakeesern@gmail.com>
2017-11-17 13:38:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,35 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+// function for loading each image via XHR
+
+function imgLoad(imgJSON) {
+  // return a promise for an image loading
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.open('GET', imgJSON.url);
+    request.responseType = 'blob';
+
+    request.onload = function() {
+      if (request.status == 200) {
+        var arrayResponse = [];
+        arrayResponse[0] = request.response;
+        arrayResponse[1] = imgJSON;
+        resolve(arrayResponse);
+      } else {
+        reject(Error('Image didn\'t load successfully; error code:' + request.statusText));
+      }
+    };
+
+    request.onerror = function() {
+      reject(Error('There was a network error.'));
+    };
+
+    // Send the request
+    request.send();
+  });
+}
+
 var imgSection = document.querySelector('section');
 
 window.onload = function() {
@@ -32,7 +61,7 @@ window.onload = function() {
 
       myImage.src = imageURL;
       myImage.setAttribute('alt', arrayResponse[1].alt);
-      myCaption.innerHTML = '<strong>' + arrayResponse[1].name + '</strong>';
+      myCaption.innerHTML = '<strong>' + arrayResponse[1].name + '</strong>: Taken by ' + arrayResponse[1].credit;
 
       imgSection.appendChild(myFigure);
       myFigure.appendChild(myImage);
