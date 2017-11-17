f14d0c4b72b6ba26c95eea3437af8ce2217416b5
Kee Sern <chuakeesern@gmail.com>
2017-11-17 13:15:43 -0500

    Update image-list.js

--- image-list.js
+++ image-list.js
@@ -2,15 +2,8 @@ var Path = 'gallery/';
 
 var Gallery = { 'images' : [
   {
-    'name'  : 'Lunch',
-    'alt' : 'You will always have your lunch.',
-    'url': 'gallery/lunchBox.jpg',
-  },
-
-  {
     'name'  : 'Jason Deli',
     'alt' : 'But do you always have Jason\'s deli signature sub?',
     'url': 'gallery/jasonDeli.jpg',
-  },
-  
+  }
 ]};
